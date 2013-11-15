2f5fd2e1122a6a78b31c908f55f6e36edffccd2a
unknown <jfrost@REA0WS0128.emea.corp.jwt.com>
2013-11-15 12:58:45 +0000

    added grunt-plato

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,16 @@
-Gruntfile.js
+module.exports = function(grunt){
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		plato: {
+			mmm: {
+				files: {
+					'reports': ['scripts/main.js', 'scripts/router.js', 'scripts/app/**/*.js']
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-plato');
+	
+};
