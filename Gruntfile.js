c2a48bf98affc61f0455a666a4ca215d6d7489aa
Jon Frost <jonfrost43@gmail.com>
2015-07-05 17:20:58 +0100

    added sass and watch tasks. some new search results list styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,38 @@ module.exports = function(grunt){
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+
 		plato: {
 			mmm: {
 				files: {
 					'reports': ['scripts/main.js', 'scripts/router.js', 'scripts/app/**/*.js']
 				}
 			}
+		},
+
+		watch: {
+			sass: {
+				files: ['sass/**/*.{scss,sass}','sass/_partials/**/*.{scss,sass}'],
+				tasks: ['sass:dist']
+			}
+		},
+
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded'
+				},
+				files: {
+					'style/main.css': 'sass/main.scss'
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-plato');
-	
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+
+	grunt.registerTask('default', ['sass:dist', 'watch']);
+
 };
