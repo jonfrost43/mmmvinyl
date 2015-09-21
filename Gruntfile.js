49841bd31832ecc447c1705b5e270520330ec3f7
Jon Frost <jonfrost43@gmail.com>
2015-09-21 19:06:08 +0100

    trying grunt-sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,8 @@ module.exports = function(grunt){
 
 	grunt.loadNpmTasks('grunt-plato');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-sass');
+	//grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-sass');
 
 	grunt.registerTask('default', ['sass:dist']);
 
