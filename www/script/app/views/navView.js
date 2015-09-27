3ef8ee737a27b10fa6d4f7014473acfc7b1ed295
Jon Frost <jonfrost43@gmail.com>
2015-09-27 17:21:15 +0100

    bootstrap the logged out nav to the homepage

--- www/script/app/views/navView.js
+++ www/script/app/views/navView.js
@@ -2,22 +2,17 @@ define([
 	'backbone',
 	'doT',
 	'text!templates/nav.html',
-], 
+],
 function(Backbone, doT, NavTemplate){
 	return Backbone.View.extend({
 		tagName: 'ul',
 		events: {
-			'click a.internal': 'goToLink'			
+			'click a.internal': 'goToLink'
 		},
 		initialize: function(){
-			console.log('app view init');
-			
 			$.ajax({
 				url: '/api/identity',
 				dataType: 'json',
-				error: _.bind(function(jqXHR){
-					this.render({username: null});
-				}, this),
 				success: _.bind(function(data){
 					this.render({username: data.username});
 				}, this)
@@ -26,7 +21,7 @@ function(Backbone, doT, NavTemplate){
 		render: function(userData){
 			var templateFnc = doT.template(NavTemplate),
 				html = templateFnc(userData);
-				
+
 			this.$el.append(html);
 			$('nav').html(this.$el);
 		},
